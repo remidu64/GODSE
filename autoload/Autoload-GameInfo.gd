543f9195fa23381bbca0df5feb10dbc2c859bf35
CyanActSus420 <cyanactsus@gmail.com>
2025-04-07 12:27:35 +0200

    options ui scaling

--- autoload/Autoload-GameInfo.gd
+++ autoload/Autoload-GameInfo.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var gameVersion = "0.2"
+var gameVersion = "0.3"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- scripts/substates/SubState-OptionsMenu.gd
+++ scripts/substates/SubState-OptionsMenu.gd
@@ -1,13 +1,14 @@
 extends Control
 
-@onready var tab_container: TabContainer = $ActualOptions/TabContainer
+@onready var tab_container: TabContainer = $TabContainer
 
 @export var inGame:bool = false
 
 func exit_menu():
 	for i in tab_container.get_children():
 		for u in i.get_children():
-			u.save_option() # colon three
+			for r in u.get_children():
+				r.save_option() # ill fix this shit later i just need it to work for now
 	
 	if !inGame:
 		pass # will change all this when we have a main menu and shit like that ykyk
