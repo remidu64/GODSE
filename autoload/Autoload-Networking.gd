e294a2fbe7b17a0116ad4fe1ea2ac1eabdceca17
CyanActSus420 <cyanactsus@gmail.com>
2025-04-07 13:09:12 +0200

    player disconnecting

--- autoload/Autoload-Networking.gd
+++ autoload/Autoload-Networking.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal add_player_request(id)
+signal remove_player_request(id)
 
 var enetPeer = ENetMultiplayerPeer.new()
 
@@ -10,12 +11,16 @@ func host_server():
 	enetPeer.create_server(PORT)
 	multiplayer.multiplayer_peer = enetPeer
 	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
 	await Global.play_state_loaded
 	add_player(multiplayer.get_unique_id())
 
 func join_server():
 	enetPeer.create_client("localhost", PORT)
 	multiplayer.multiplayer_peer = enetPeer
+
+func remove_player(id):
+	remove_player_request.emit(id)
 
 func add_player(id):
 	add_player_request.emit(id)
--- scripts/State-PlayState.gd
+++ scripts/State-PlayState.gd
@@ -6,11 +6,17 @@ const Player = preload("res://scenes/entities/Entity-Player.tscn")
 
 func _ready():
 	Networking.add_player_request.connect(add_player)
+	Networking.remove_player_request.connect(remove_player)
 	Options.changed.connect(toggle_shadows)
 	
 	Global.play_state_loaded.emit()
 	
 	toggle_shadows()
+
+func remove_player(id):
+	var player = get_node_or_null(str(id))
+	if player:
+		player.queue_free()
 
 func add_player(id):
 	var newPlayer = Player.instantiate()
