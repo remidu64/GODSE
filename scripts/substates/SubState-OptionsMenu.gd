63d2052eba86af27aaaec76b9ac15000d8be4fce
CyanActSus420 <cyanactsus@gmail.com>
2025-04-07 12:05:13 +0200

    settings tabs

--- scripts/substates/SubState-OptionsMenu.gd
+++ scripts/substates/SubState-OptionsMenu.gd
@@ -1,12 +1,13 @@
 extends Control
 
-@onready var actualOptions: Control = $ActualOptions
+@onready var tab_container: TabContainer = $ActualOptions/TabContainer
 
 @export var inGame:bool = false
 
 func exit_menu():
-	for i in actualOptions.get_children():
-		i.save_option()
+	for i in tab_container.get_children():
+		for u in i.get_children():
+			u.save_option() # colon three
 	
 	if !inGame:
 		pass # will change all this when we have a main menu and shit like that ykyk
