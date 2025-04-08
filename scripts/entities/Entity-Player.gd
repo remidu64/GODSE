3392b0668562482519c6845ab418b5524bf064b8
remidu64 <stanydumont76@gmail.com>
2025-04-08 13:29:12 +0200

    added a lil air control

--- scripts/entities/Entity-Player.gd
+++ scripts/entities/Entity-Player.gd
@@ -13,9 +13,9 @@ const ACCELERATION = 1.1
 const RUNNING_MULTIPLIER = 1.5
 const JUMP_VELOCITY = 5.5
 const JUMP_SPEED_MULTIPLIER = 1.35
-const LATERAL_VELOCITY_COEFFICENT = 0.15 # How much does moving impact how high you jump
+const LATERAL_VELOCITY_COEFFICENT = 0.1 # How much does moving impact how high you jump
 const FRICTION = 0.35
-const AIR_FRICTION = 0.0
+const AIR_FRICTION = 0.01
 
 
 const SUB_STATE_OPTIONS_MENU = preload("res://scenes/substates/SubState-OptionsMenu.tscn")
@@ -90,6 +90,9 @@ func _physics_process(delta: float) -> void:
 		if direction and is_on_floor():
 			velocity.x += ACCELERATION * direction.x
 			velocity.z += ACCELERATION * direction.z
+		elif direction:
+			velocity.x += ACCELERATION * direction.x * 0.05
+			velocity.z += ACCELERATION * direction.z * 0.05
 			
 	# p h y s i c s
 	move_and_slide()
