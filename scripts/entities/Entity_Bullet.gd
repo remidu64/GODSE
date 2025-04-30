7587fbe270d0b66daae4a086d648ca5adabeb925
remidu64 <stanydumont76@gmail.com>
2025-04-30 16:43:02 +0200

    owo

--- scripts/entities/Entity_Bullet.gd
+++ scripts/entities/Entity_Bullet.gd
@@ -1,14 +1,19 @@
 extends RigidBody3D
 
-@onready var tester: MeshInstance3D = $test
+@onready var tester: Node3D = $test
+
+var diff = Vector3(0, 0, 0)
 
 func _ready() -> void:
 	set_contact_monitor(true)
 	set_max_contacts_reported(10)
 	
 func _physics_process(delta: float) -> void:
-	look_at(tester.global_position)
-	tester.global_position = linear_velocity + global_position
+	if linear_velocity.length() > 5:
+		tester.look_at(global_transform.origin + linear_velocity)
+	diff = (tester.rotation - rotation)
+	print(diff)
+		
 	if position.y < -100:
 		queue_free()
 	for i in get_colliding_bodies():
@@ -16,9 +21,9 @@ func _physics_process(delta: float) -> void:
 			queue_free()
 			i.health -= 20
 		if i.is_in_group("map"):
-			pass
-			# queue_free()
+			queue_free()
 			
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	pass 
+	if linear_velocity.length() > 5:
+		apply_torque_impulse(diff*global_transform.basis.inverse())
 	# apply_torque_impulse(Vector3(0, 0, 1) * global_transform.basis.inverse())
