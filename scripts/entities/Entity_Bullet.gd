69ff6856d913f5119dbf72084051ab7c2fe37269
remidu64 <stanydumont76@gmail.com>
2025-04-30 13:23:08 +0200

    im going insane

--- scripts/entities/Entity_Bullet.gd
+++ scripts/entities/Entity_Bullet.gd
@@ -1,10 +1,14 @@
 extends RigidBody3D
 
+@onready var tester: MeshInstance3D = $test
+
 func _ready() -> void:
 	set_contact_monitor(true)
 	set_max_contacts_reported(10)
 	
 func _physics_process(delta: float) -> void:
+	look_at(tester.global_position)
+	tester.global_position = linear_velocity + global_position
 	if position.y < -100:
 		queue_free()
 	for i in get_colliding_bodies():
@@ -16,4 +20,5 @@ func _physics_process(delta: float) -> void:
 			# queue_free()
 			
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	pass
+	pass 
+	# apply_torque_impulse(Vector3(0, 0, 1) * global_transform.basis.inverse())
