3074f26b48a5d6ca79bfeda9edd4a6676ce3cba5
remidu64 <stanydumont76@gmail.com>
2025-05-16 23:41:57 +0200

    adding modular guns

--- autoload/Autoload-Guns.gd
+++ autoload/Autoload-Guns.gd
@@ -1,1 +1,16 @@
-extends Node
+extends Node3D
+
+var damage: float
+var muzzle_velocity: float
+	
+var reload_time: float
+var mag_size: int
+	
+var ads_zoom: float
+	
+var model
+
+func init(_damage, _muzzle_velocity, _model):
+	damage = _damage
+	muzzle_velocity = _muzzle_velocity
+	model = _model
