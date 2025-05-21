373c218b89c371309bf5f092314b6b5fa9303678
remidu64 <stanydumont76@gmail.com>
2025-05-21 23:44:07 +0200

    gotta sync randomness between player 🥀

--- scripts/entities/Entity_Server.gd
+++ scripts/entities/Entity_Server.gd
@@ -16,3 +16,13 @@ func _physics_process(delta: float) -> void:
 	if not multiplayer.is_server():
 		return
 	playercount.text = "Current Player Count: %s" % get_tree().get_nodes_in_group("players").size()
+
+func get_spread_dir(camera_global_basis : Basis, spread_degrees: float) -> Vector3:
+	var twist : float = randf_range(0, TAU)
+	var axis := Vector3(cos(twist), sin(twist), 0)
+  
+	var angle := (1.0 - sqrt(1.0 - sqrt(randf()))) * deg_to_rad(spread_degrees) # Superior distribution
+  #var angle := sqrt(randf()) * spread_degrees # Uniform spread, use instead if prefered
+  
+  # Negative because cameras in Godot point backwards
+	return -camera_global_basis.z.rotated(camera_global_basis * axis, angle)
